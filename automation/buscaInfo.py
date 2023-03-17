66a9893c191846938be96b06317f361a5b4ec013
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2023-03-17 06:13:37 -0300

    Retirada de laço desnecessário

--- automation/buscaInfo.py
+++ automation/buscaInfo.py
@@ -9,16 +9,15 @@ lista_estacoes = [x[:-1] for x in estacoes]
 
 url = 'https://www.viamobilidade.com.br/nos/linha-9-esmeralda/'
 
+lista1 = []
+lista2 = []
+
 for x in lista_estacoes:
     pagina = urllib.request.urlopen(url+x)
     with open('enderecos.txt', 'a', encoding='UTF-8') as file:
         file.write(f"{x} = {str(BeautifulSoup(pagina, 'html5lib', from_encoding='UTF-8').find_all('p')[2]).replace('<p>', '').replace('</p>', '')} \n")
         # Estação mendes vila natal não irá constar endereço. Colocar manualmente = Estrada dos Mendes, s/n - Jardim Icarai – Grajaú - 04860-140
     
-lista1 = []
-lista2 = []
-for x in lista_estacoes:
-    pagina = urllib.request.urlopen(url+x)
     retorno = str(BeautifulSoup(pagina, 'html5lib').findAll('span')[2:-1]).replace('<span>', '').replace('</span>', '').replace('<br/>', ' - ')
     
     with open('caracteristicas.txt', 'a', encoding='UTF-8') as file:
