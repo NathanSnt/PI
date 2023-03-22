245cb64e26ce68d9a7b949459d1871277d3e3b50
NathanSnt <natan-silva-2011@hotmail.com>
2023-03-21 23:31:47 -0300

    corrigido bug resize no botão info comentário

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -30,26 +30,15 @@ function mudarConteudo(pagina, divId, carrossel, nome_estacao) {
 }
 
 function mostrarInfo(objeto) {
-    // document.querySelector('#comentario-footer')
     const divComentarioFooter = objeto.parentElement.children[1].children[1]
-    if (divComentarioFooter.style.display == 'block') {
-        console.log(divComentarioFooter.style.display)
-        divComentarioFooter.style.display = ''
-    }else {
-        console.log(divComentarioFooter.style.display)
-        divComentarioFooter.style.display = 'block'
-    }
+    divComentarioFooter.classList.toggle('mostrarInfo')
 }
 
 function mostrarMenu() {
-    //let divMenuEstacao = document.querySelector('#menu-estacoes')
-    //if (document.querySelector('#menu-estacoes').style.display === 'none'){
     if (document.querySelector('#menu-estacoes').offsetWidth === 0) {
-        console.log('on')
         ativaMenu()
     }
     else {
-        console.log('off')
         desativaMenu()
     }
 }
@@ -60,20 +49,11 @@ function ativaMenu() {
     let divConteudo = document.querySelector("#conteudo")
     let faixa = document.querySelector("#faixa")
 
-    // divMenuEstacao.style.setProperty('display', 'block', 'important')
-    //divMenuEstacao.style.width = '50%'
-    // divMenuEstacao.style.backgroundColor = '#eee'
-    // divMenuEstacao.style.zIndex = '-3'
     divMenuEstacao.classList.add('menuAtivo')
     divConteudo.style.display = 'none'
     faixa.classList.add('faixaMenuHamburger')
     estacao.classList.add('estacaoMenuHamburger')
-    //faixa.style.display = 'block'
     atualizaTamanhoFaixa()
-    //faixa.style.left = '15%'
-    //faixa.style.width = '4.5%'
-    //faixa.style.zIndex = '-2'
-    //estacao.style.zIndex = '-1'
 }
 
 function desativaMenu() {
@@ -85,17 +65,8 @@ function desativaMenu() {
     divMenuEstacao.classList.remove('menuAtivo')
     faixa.classList.remove('faixaMenuHamburger')
     estacao.classList.remove('estacaoMenuHamburger')
-    //divMenuEstacao.style.setProperty('display', 'none', 'important')
-    //divMenuEstacao.style.setProperty('background-color', '#fff', 'important')
-    //faixa.style.display = 'block'
     divConteudo.style.setProperty('display', 'block', 'important')
 }
-
-// $('#sobre_nos').click(function() {
-//     $.get('sobre', function(data) {
-//         $('#conteudo').html(data)
-//     })
-// })
 
 window.addEventListener('load', function() {
     atualizaTamanhoFaixa()
@@ -103,33 +74,9 @@ window.addEventListener('load', function() {
 window.addEventListener('resize', function() {
     atualizaTamanhoFaixa()
     desativaMenu()
+    this.document.querySelectorAll('#comentario-footer').forEach(div => {
+        if (div.classList.contains('mostrarInfo')) {
+            div.classList.toggle('mostrarInfo')
+        }
+    })
 })
-
-
-
-
-
-// LISTENER RESIZE
-// let menuEstacao = this.document.querySelector("menu-estacoes").style
-    // let faixa = this.document.querySelector("faixa").style
-    // let conteudo = this.document.querySelector("conteudo").style
-    // let hamburger = this.document.querySelector("menu-hamburger").style
-   // if (this.window.width <= 600){
-        // menuEstacao.display = 'none '
-        // conteudo.width = '90%'
-        // conteudo.margin = 'auto'
-        // hamburger.display = 'block'
-        // hamburger.width = '7.5%'
-        // hamburger.height = '50px'
-        // hamburger.marginLeft = '2.5%'
-        // faixa.display = 'none'
-    //    ativaMenu()
-   // }
-   // else if (this.window.width > 600){
-        // hamburger.display = 'none'
-        // menuEstacao.display = 'block'
-        // menuEstacao.backgroundColor = 'white'
-        // faixa.display = 'block'
-        // conteudo.display = 'block'
-      //  desativaMenu()
-   // }
