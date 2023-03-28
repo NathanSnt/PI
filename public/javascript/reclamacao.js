d812245cf22cd43e29e0e2ba70209435bf15904a
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2023-03-28 14:22:12 -0300

    melhoria na responsividade e estilo

--- public/javascript/reclamacao.js
+++ public/javascript/reclamacao.js
@@ -1,39 +1,39 @@
-// let radio_estacao = document.querySelector('#radio-estacao');
-// let radio_carro = document.querySelector('#radio-carro');
-// let radio_linha = document.querySelector('#radio-linha');
-// let div_linha_geral = document.querySelector('#linha-geral');
-// let div_estacao_especifica = document.querySelector('#estacao-especifica');
-// let div_carro_especifico = document.querySelector('#carro-especifico');
 
-// radio_estacao.addEventListener('change', () => {
 document.querySelector('#radio-estacao').addEventListener('change', () => {
     let div_linha_geral = document.querySelector('#linha-geral');
     let div_estacao_especifica = document.querySelector('#estacao-especifica');
     let div_carro_especifico = document.querySelector('#carro-especifico');
     div_estacao_especifica.style.display = 'block';
+    document.querySelector("#radioEstacao").classList.toggle('selecionado')
     div_linha_geral.style.display = 'none';
+    document.querySelector("#radioLinha").classList.remove('selecionado')
     div_carro_especifico.style.display = 'none';
+    document.querySelector("#radioCarro").classList.remove('selecionado')
     console.log("estação específica selecionado")
 });
 
-// radio_carro.addEventListener('change', () => {
 document.querySelector('#radio-carro').addEventListener('change', () => {
     let div_linha_geral = document.querySelector('#linha-geral');
     let div_estacao_especifica = document.querySelector('#estacao-especifica');
     let div_carro_especifico = document.querySelector('#carro-especifico');
     div_carro_especifico.style.display = 'block';
+    document.querySelector("#radioCarro").classList.add('selecionado')
     div_linha_geral.style.display = 'none';
+    document.querySelector("#radioLinha").classList.remove('selecionado')
     div_estacao_especifica.style.display = 'none';
+    document.querySelector("#radioEstacao").classList.remove('selecionado')
     console.log("Carro específico selecionado")
 });
 
-// radio_linha.addEventListener('change', () => {
 document.querySelector('#radio-linha').addEventListener('change', () => {
     let div_linha_geral = document.querySelector('#linha-geral');
     let div_estacao_especifica = document.querySelector('#estacao-especifica');
     let div_carro_especifico = document.querySelector('#carro-especifico');
     div_linha_geral.style.display = 'block';
+    document.querySelector("#radioLinha").classList.toggle('selecionado')
     div_estacao_especifica.style.display = 'none';
+    document.querySelector("#radioEstacao").classList.remove('selecionado')
     div_carro_especifico.style.display = 'none';
+    document.querySelector("#radioCarro").classList.remove('selecionado')
     console.log("Linha em geral selecionado")
 });
